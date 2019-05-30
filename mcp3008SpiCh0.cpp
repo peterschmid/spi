f70f46c5954f9f886d39cb5fa8f43286652e3fad
peterschmid <peter.r.schmid@gmail.com>
2019-05-30 12:48:32 +0200

    Corrected calculation and added config file

--- mcp3008SpiCh0.cpp
+++ mcp3008SpiCh0.cpp
@@ -1,23 +1,45 @@
 #include <unistd.h>
 #include <iostream>
+#include <fstream>
 #include "mcp3008Spi.h"
 
 using namespace std;
 
 /**
- * Main program to read value from mcp3008 channel 0 and calculate level
+ * Main program to read value from mcp3008 channel 0 and calculate level in cm
  */
 int main(void)
 {
+    // read config file
+    ifstream in("waterlevel.conf");
+    std::string line;
+    const std::string separator("=");
+    double a(0.0);
+    double b(0); 
+    int t(0);
+    for(auto i=0; i<3; ++i) {
+        std::getline(in, line);
+        auto separatorPos = line.find(separator);
+        auto key = line.substr(0, separatorPos);
+        auto val = line.substr(separatorPos+1, std::string::npos);
+        if (key=="a") {
+             a = std::stod(val);
+        } else if (key=="b")  {
+             b = std::stod(val);
+        } else if (key=="t") {
+             t = std::stoi(val);
+        }
+    }
+    in.close();
+
+    // read sensor value
     mcp3008Spi a2d("/dev/spidev0.0", SPI_MODE_0, 1000000, 8);
 
     auto digit = a2d.readValueChannel0();
-    // change form digits to cm water level
-    auto level = digit*0.3478-63.702;
-    auto intLevel = (int)(level+0.5);
-    if (intLevel<0) {
-        intLevel=0;
-    }
+    // change form digits to cm water level (function is based on measurement)
+    auto level = digit*a+b;
+    // round and add to built in depth (60cm)
+    auto intLevel = t + (int)(level+0.5);
     cout << intLevel << endl;
     return 0;
 }
--- mcp3008SpiTestCh0.cpp
+++ mcp3008SpiTestCh0.cpp
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <iostream>
+#include <fstream>
 #include "mcp3008Spi.h"
 
 using namespace std;
@@ -9,6 +10,30 @@ using namespace std;
  */
 int main(void)
 {
+    std::cout<<"Read waterlevel.conf"<<std::endl;
+    ifstream in("waterlevel.conf");
+    std::string line;
+    const std::string separator("=");
+    double a(0.0);
+    double b(0); 
+    int t(0);
+    for(auto i=0; i<3; ++i) {
+        std::getline(in, line);
+        auto separatorPos = line.find(separator);
+        std::string key = line.substr(0, separatorPos);
+        std::string val = line.substr(separatorPos+1, std::string::npos);
+        std::cout << key << ":" << val << std::endl;
+        if (key=="a") {
+             a = std::stod(val);
+        } else if (key=="b")  {
+             b = std::stod(val);
+        } else if (key=="t") {
+             t = std::stoi(val);
+        }
+        std::cout << "a=" <<  a << ", b=" << b << ", t=" << t << std::endl;
+    }
+    in.close();
+
     mcp3008Spi a2d("/dev/spidev0.0", SPI_MODE_0, 1000000, 8);
     int i = 20;
 
@@ -16,7 +41,7 @@ int main(void)
     {
     	auto digit = a2d.readValueChannel0();
     	// change form digits to cm water level
-    	auto level = digit*0.3478-63.702;
+    	auto level = digit*a+b;
     	cout << "Ch0: Digit: " << digit << ", Pegel: " << level << "cm" << endl;
         sleep(1);
         i--;
